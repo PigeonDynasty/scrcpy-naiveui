97ba95f5ee93aaa8a2203479e02c30e0b3a681e4
BookFlower <1753980101@qq.com>
2021-12-12 16:52:26 +0800

    Change UI

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,52 +1,117 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
-  import {NCard,NTabs,NTabPane} from 'naive-ui'
+import { defineComponent,ref } from 'vue'
+  import {NCard,NTabs,NTabPane,NInput,NForm,NFormItemRow,NButton,NSpace,NSwitch,
+  NCheckbox,NSlider,NInputNumber,NSelect,NCheckboxGroup,NDivider} from 'naive-ui'
   export default defineComponent({
     components: {
-    NCard,NTabs,NTabPane
-    },
+    NCard,NTabs,NTabPane,NInput,NForm,NFormItemRow,NButton,NSpace,NSwitch,
+    NCheckbox,NSlider,NInputNumber,NSelect,NCheckboxGroup,NDivider},
     setup() {
       return {
-
+        active: ref(false),
+        value: ref(false),
+        BitRate: ref(0),
+        resolution: ref(0),
+        Framerate: ref(0),
+        angle: ref(null),
+        options: [
+        {
+          label: "0°",
+          value: 'angle0',
+        },
+        {
+          label: '90°',
+          value: 'angle1'
+        },
+        {
+          label: '180°',
+          value: 'angle2'
+        },
+        {
+          label: "270°",
+          value: 'angle3',
+        }
+        ],
+        settings: ref(null)
       }
     }
   })
 </script>
 
 <template>
-  <n-card title="歌曲" style="margin-bottom: 16px;">
-    <n-tabs type="line">
-      <n-tab-pane name="oasis" tab="Oasis">Wonderwall</n-tab-pane>
-      <n-tab-pane name="the beatles" tab="the Beatles">Hey Jude</n-tab-pane>
-      <n-tab-pane name="jay chou" tab="周杰伦">七里香</n-tab-pane>
-    </n-tabs>
-  </n-card>
-  <n-card>
-    <n-tabs default-value="signin" size="large">
-      <n-tab-pane name="signin" tab="登录">
+  <n-card title="Scrcpy" style="margin-bottom: 16px;">
+    <n-tabs default-value="Mirror configuration" justify-content="space-evenly" size="large">
+      <n-tab-pane name="Mirror configuration" tab="镜像配置">
         <n-form>
-          <n-form-item-row label="用户名">
-            <n-input />
+          <n-form-item-row label="窗口标题">
+            <n-input placeholder="请输入窗口标题"/>
           </n-form-item-row>
-          <n-form-item-row label="密码">
-            <n-input />
+          <n-form-item-row label="Scrcpy">
+            <n-input placeholder="Scrcpy文件夹路径-例如:C:\scrcpy-win64" />
           </n-form-item-row>
+          <n-form-item-row label="镜像录屏">
+           <n-space>
+             <n-switch v-model:value="active" />
+             <n-checkbox v-model:checked="value">录屏时打开镜像</n-checkbox>
+           </n-space>
+          </n-form-item-row>
+           <n-form-item-row label="录屏文件路径">
+            <n-input placeholder="请输入录屏文件路径"/>
+          </n-form-item-row>
+          <n-form-item-row label="镜像传输比特率">
+           <n-space vertical>
+              <n-slider v-model:value="BitRate" :step="1" :max="1024" />
+              <n-input-number size="small" v-model:value="BitRate"  :min="0"/>
+           </n-space>
+          </n-form-item-row>
+          <n-form-item-row label="等比最大分辨率">
+           <n-space vertical>
+              <n-slider v-model:value="resolution" :step="1" :max="1024" />
+              <n-input-number size="small" v-model:value="resolution"  :min="0"/>
+           </n-space>
+          </n-form-item-row>
+          <n-form-item-row label="最大帧率">
+           <n-space vertical>
+              <n-slider v-model:value="Framerate" :step="1" :max="144" />
+              <n-input-number size="small" v-model:value="Framerate"  :min="0"/>
+           </n-space>
+          </n-form-item-row>
+          <n-form-item-row label="旋转角度">
+             <n-select v-model:value="angle" :options="options" />
+          </n-form-item-row>
+          <n-form-item-row label="其他设置">
+             <n-checkbox-group v-model:value="settings">
+              <n-space item-style="display: flex;">
+                <n-checkbox value="zhiding" label="窗口置顶" />
+                <n-checkbox value="kongzhi" label="电脑控制" />
+                <n-checkbox value="biankuang" label="显示边框" />
+                <n-checkbox value="quanping" label="全屏显示" />
+                <n-checkbox value="guanbisuoping" label="关闭锁屏" />
+                <n-checkbox value="xuanran" label="渲染所有帧 会增加延迟" />
+                <n-checkbox value="guanbipingmu" label="打开镜像时关闭屏幕" />
+              </n-space>
+             </n-checkbox-group>
+          </n-form-item-row>
+           <n-divider />
+          <n-space>
+            <n-button type="info">保存当前配置</n-button>
+            <n-button type="primary">恢复默认配置</n-button>
+           
+  </n-space>
         </n-form>
-        <n-button type="primary" block>登录</n-button>
+      
       </n-tab-pane>
-      <n-tab-pane name="signup" tab="注册">
+      <n-tab-pane name="Image management" tab="镜像管理">
         <n-form>
-          <n-form-item-row label="用户名">
-            <n-input />
+          <n-divider >设备局域网IP地址</n-divider>
+          <n-form-item-row >
+           <n-input placeholder="请输入IP地址" />
           </n-form-item-row>
-          <n-form-item-row label="密码">
-            <n-input />
-          </n-form-item-row>
-          <n-form-item-row label="重复密码">
-            <n-input />
+            <n-button type="primary">开启无线连接</n-button>
+            <n-divider ></n-divider>
+          <n-form-item-row label="暂无设备连接">
           </n-form-item-row>
         </n-form>
-        <n-button type="primary" block>注册</n-button>
       </n-tab-pane>
     </n-tabs>
   </n-card>
@@ -54,6 +119,6 @@ import { defineComponent } from 'vue'
 
 <style>
 body {
-    background: white;
+    background: rgb(102, 96, 96);
   }
 </style>
